228a7728640af16153d8813c89f21a6183e59de9
Vladimir <vladimircuraciov@gmail.com>
2020-06-27 12:50:57 +0300

    ic_launcher fixed

--- MoneyNote.Android/MainActivity.cs
+++ MoneyNote.Android/MainActivity.cs
@@ -6,7 +6,7 @@ using Android.Runtime;
 
 namespace MoneyNote.Droid
 {
-    [Activity(Label = "MoneyNote", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "MoneyNote", Icon = "@mipmap/ic_launcher", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
