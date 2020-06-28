3fad94789aa549b199855c7cdc21154fe09930de
Vladimir <vladimircuraciov@gmail.com>
2020-06-28 20:06:28 +0300

    About & Terms View fixed. changed orientation.

---
+++ MoneyNote/Styles/ButtonStyles.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MoneyNote.Styles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ButtonStyles : ResourceDictionary
+    {
+        public ButtonStyles()
+        {
+            InitializeComponent();
+        }
+    }
+}
