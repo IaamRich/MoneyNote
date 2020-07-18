d04b6eeb329c15122670f80b8d2e33a5ce9548bc
Vladimir <vladimircuraciov@gmail.com>
2020-07-18 04:28:30 +0300

    Added Logo, changed color palette.

--- MoneyNote.Android/Resources/Resource.designer.cs
+++ MoneyNote.Android/Resources/Resource.designer.cs
@@ -5823,28 +5823,31 @@ namespace MoneyNote.Droid
 			public const int notify_panel_notification_icon_bg = 2131165311;
 			
 			// aapt resource value: 0x7F070080
-			public const int ru = 2131165312;
+			public const int plus = 2131165312;
 			
 			// aapt resource value: 0x7F070081
-			public const int settings = 2131165313;
+			public const int ru = 2131165313;
 			
 			// aapt resource value: 0x7F070082
-			public const int splash = 2131165314;
+			public const int settings = 2131165314;
 			
 			// aapt resource value: 0x7F070083
-			public const int terms = 2131165315;
+			public const int splash = 2131165315;
 			
 			// aapt resource value: 0x7F070084
-			public const int tooltip_frame_dark = 2131165316;
+			public const int terms = 2131165316;
 			
 			// aapt resource value: 0x7F070085
-			public const int tooltip_frame_light = 2131165317;
+			public const int tooltip_frame_dark = 2131165317;
 			
 			// aapt resource value: 0x7F070086
-			public const int xamarin_logo = 2131165318;
+			public const int tooltip_frame_light = 2131165318;
 			
 			// aapt resource value: 0x7F070087
-			public const int zh = 2131165319;
+			public const int xamarin_logo = 2131165319;
+			
+			// aapt resource value: 0x7F070088
+			public const int zh = 2131165320;
 			
 			static Drawable()
 			{
--- MoneyNote/ViewModels/AboutViewModel.cs
+++ MoneyNote/ViewModels/AboutViewModel.cs
@@ -1,14 +1,20 @@
-﻿using I18NPortable;
+﻿using System;
+using System.Windows.Input;
+using I18NPortable;
 using ReactiveUI;
+using Xamarin.Essentials;
 
 namespace MoneyNote
 {
     public class AboutViewModel : ReactiveObject, IRoutableViewModel
     {
         public II18N Strings => I18N.Current;
+        public ICommand OpenWebCommand { get; set; }
+        public ICommand VisitCompanySite { get; set; }
         public AboutViewModel()
         {
-
+            OpenWebCommand = ReactiveCommand.Create(() => { Launcher.OpenAsync(new Uri(@"https://curaciov.com")); });
+            VisitCompanySite = ReactiveCommand.Create(() => { Launcher.OpenAsync(new Uri(@"https://sapec.md")); });
         }
 
         public string UrlPathSegment => Strings["menu_about"];
