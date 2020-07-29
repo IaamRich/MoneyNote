92c04cad6343b2665bca0711926be02cc42ef789
Vladimir <vladimircuraciov@gmail.com>
2020-07-29 09:44:09 +0300

    started adding hands

--- MoneyNote.Android/Resources/Resource.designer.cs
+++ MoneyNote.Android/Resources/Resource.designer.cs
@@ -10934,91 +10934,97 @@ namespace MoneyNote.Droid
 			public const int it = 2131165297;
 			
 			// aapt resource value: 0x7F070072
-			public const int logo = 2131165298;
+			public const int left_image_hand = 2131165298;
 			
 			// aapt resource value: 0x7F070073
-			public const int main = 2131165299;
+			public const int logo = 2131165299;
 			
 			// aapt resource value: 0x7F070074
-			public const int md = 2131165300;
+			public const int main = 2131165300;
 			
 			// aapt resource value: 0x7F070075
-			public const int mtrl_snackbar_background = 2131165301;
+			public const int md = 2131165301;
 			
 			// aapt resource value: 0x7F070076
-			public const int mtrl_tabs_default_indicator = 2131165302;
+			public const int mtrl_snackbar_background = 2131165302;
 			
 			// aapt resource value: 0x7F070077
-			public const int navigation_empty_icon = 2131165303;
+			public const int mtrl_tabs_default_indicator = 2131165303;
 			
 			// aapt resource value: 0x7F070078
-			public const int notification_action_background = 2131165304;
+			public const int navigation_empty_icon = 2131165304;
 			
 			// aapt resource value: 0x7F070079
-			public const int notification_bg = 2131165305;
+			public const int notification_action_background = 2131165305;
 			
 			// aapt resource value: 0x7F07007A
-			public const int notification_bg_low = 2131165306;
+			public const int notification_bg = 2131165306;
 			
 			// aapt resource value: 0x7F07007B
-			public const int notification_bg_low_normal = 2131165307;
+			public const int notification_bg_low = 2131165307;
 			
 			// aapt resource value: 0x7F07007C
-			public const int notification_bg_low_pressed = 2131165308;
+			public const int notification_bg_low_normal = 2131165308;
 			
 			// aapt resource value: 0x7F07007D
-			public const int notification_bg_normal = 2131165309;
+			public const int notification_bg_low_pressed = 2131165309;
 			
 			// aapt resource value: 0x7F07007E
-			public const int notification_bg_normal_pressed = 2131165310;
+			public const int notification_bg_normal = 2131165310;
 			
 			// aapt resource value: 0x7F07007F
-			public const int notification_icon_background = 2131165311;
+			public const int notification_bg_normal_pressed = 2131165311;
 			
 			// aapt resource value: 0x7F070080
-			public const int notification_template_icon_bg = 2131165312;
+			public const int notification_icon_background = 2131165312;
 			
 			// aapt resource value: 0x7F070081
-			public const int notification_template_icon_low_bg = 2131165313;
+			public const int notification_template_icon_bg = 2131165313;
 			
 			// aapt resource value: 0x7F070082
-			public const int notification_tile_bg = 2131165314;
+			public const int notification_template_icon_low_bg = 2131165314;
 			
 			// aapt resource value: 0x7F070083
-			public const int notify_panel_notification_icon_bg = 2131165315;
+			public const int notification_tile_bg = 2131165315;
 			
 			// aapt resource value: 0x7F070084
-			public const int plus = 2131165316;
+			public const int notify_panel_notification_icon_bg = 2131165316;
 			
 			// aapt resource value: 0x7F070085
-			public const int ru = 2131165317;
+			public const int plus = 2131165317;
 			
 			// aapt resource value: 0x7F070086
-			public const int selected = 2131165318;
+			public const int right_image_hand = 2131165318;
 			
 			// aapt resource value: 0x7F070087
-			public const int settings = 2131165319;
+			public const int ru = 2131165319;
 			
 			// aapt resource value: 0x7F070088
-			public const int splash = 2131165320;
+			public const int selected = 2131165320;
 			
 			// aapt resource value: 0x7F070089
-			public const int terms = 2131165321;
+			public const int settings = 2131165321;
 			
 			// aapt resource value: 0x7F07008A
-			public const int tooltip_frame_dark = 2131165322;
+			public const int splash = 2131165322;
 			
 			// aapt resource value: 0x7F07008B
-			public const int tooltip_frame_light = 2131165323;
+			public const int terms = 2131165323;
 			
 			// aapt resource value: 0x7F07008C
-			public const int unselected = 2131165324;
+			public const int tooltip_frame_dark = 2131165324;
 			
 			// aapt resource value: 0x7F07008D
-			public const int xamarin_logo = 2131165325;
+			public const int tooltip_frame_light = 2131165325;
 			
 			// aapt resource value: 0x7F07008E
-			public const int zh = 2131165326;
+			public const int unselected = 2131165326;
+			
+			// aapt resource value: 0x7F07008F
+			public const int xamarin_logo = 2131165327;
+			
+			// aapt resource value: 0x7F070090
+			public const int zh = 2131165328;
 			
 			static Drawable()
 			{
