1533768dabc5034733150fd292bace04f1efca6d
Vladimir <vladimircuraciov@gmail.com>
2020-08-11 09:37:37 +0300

    added category enum types + images.

---
+++ MoneyNote/Models/TransactionType.cs
@@ -0,0 +1,16 @@
+﻿namespace MoneyNote.Models
+{
+    public enum TransactionType
+    {
+        Market,
+        Bar,
+        Transport,
+        Business,
+        Salary,
+        Network,
+        Entertainment,
+        Gift,
+        Earnings,
+        Other
+    }
+}
