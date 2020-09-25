03279385d45c01de7b2a1234c2c196ebf67fbe52
Vladimir <vladimircuraciov@gmail.com>
2020-09-25 03:51:10 +0300

    added fonts

--- MoneyNote/AssemblyInfo.cs
+++ MoneyNote/AssemblyInfo.cs
@@ -1,3 +1,6 @@
-﻿using Xamarin.Forms.Xaml;
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: ExportFont("FlowExt-Regular.otf", Alias = "MainFont")]
+[assembly: ExportFont("FlowExt-Bold.otf", Alias = "MainFontBold")]
