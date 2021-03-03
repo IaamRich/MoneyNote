8253cc83efbc2b6e0ee8a19ccf3cf102c260727c
Vladimir <vladimircuraciov@gmail.com>
2021-03-03 23:34:09 +0200

    corrected from-to translating in history

---
+++ MoneyNote/Converters/ToStringConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using I18NPortable;
+using Xamarin.Forms;
+
+namespace MoneyNote.Converters
+{
+    public class ToStringConverter : IValueConverter
+    {
+        private II18N Strings => I18N.Current;
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var result = value.ToString();
+            switch (result)
+            {
+                case "Cash":
+                    return Strings["cash"];
+                    break;
+                case "Card":
+                default:
+                    return Strings["card"];
+                    break;
+            }
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
